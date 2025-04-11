82a915ce77df034529de1121c5bd6144baba74cc
sneka2001500 <sneka.ponnuchamy@rheincs.com>
2025-04-11 11:22:12 +0530

    Add project files.

---
+++ EmailTriggerAPI/Properties/Models/EmailSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace EmailTriggerAPI.Properties.Models
+{
+    public class EmailSettings
+    {
+        public string SmtpServer { get; set; }
+        public int SmtpPort { get; set; }
+        public string SenderEmail { get; set; }
+        public string SenderPassword { get; set; }
+        public bool EnableSsl { get; set; }
+    }
+}
